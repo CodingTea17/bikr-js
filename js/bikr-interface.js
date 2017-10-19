2c2e022ed15312231e32a1d974ca2c0fecd02c2f
Dawson Mortenson and Senya Webster <senya@>
2017-10-19 10:56:02 -0700

    move search function to backend

--- js/bikr-interface.js
+++ js/bikr-interface.js
@@ -7,67 +7,20 @@ $(document).ready(() => {
   let bike = new Bikr;
 
   $('#searchClick').click(() => {
+    $("#results").show();
+    $('#returnBike').html("");
+    $("#bikeSearch").hide();
+
     let make = $('#make').val();
     let model = $('#model').val();
     let year = $('#year').val();
 
-    let promise = new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=${make}%20${model}%20${year}&location=97205&distance=25&stolenness=proximity`;
-      request.onload = () => {
-        if (request.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then((response) => {
-      let data = JSON.parse(response);
-      $("#results").show();
-      $('#returnBike').html("");
-      $("#bikeSearch").hide();
-      data["bikes"].forEach((bike) => {
-        let stolenDate = new Date(bike.date_stolen*1000)
-        let bike_color = bike.frame_colors[0];
-        switch(bike_color) {
-          case "Black":
-            bike_color = "bg-dark";
-            break;
-          case "Red":
-            bike_color = "bg-danger";
-            break;
-          case "Yellow or Gold":
-            bike_color = "bg-warning";
-            break;
-          case "Blue":
-            bike_color = "bg-primary";
-            break;
-          case "Green":
-            bike_color = "bg-success";
-            break;
-          case "Silver or Gray":
-            bike_color = "table-active text-dark";
-            break;
-          case "Orange":
-            bike_color = "bg-secondary";
-            break;
-          default:
-            bike_color = "bg-light text-dark";
-        }
-        $('#returnBike').append(`<tr class="text-white ${bike_color} "><td>${bike.manufacturer_name}</td><td>${bike.frame_model}</td><td>${bike.year}</td><td>${bike.frame_colors}</td><td><a class="bike-link" href="https://bikeindex.org/bikes/${bike.id}">${bike.serial}</a></td><td>${stolenDate.getMonth() + 1}/${stolenDate.getDate()}/${stolenDate.getFullYear()}</td></tr>`);
-      });
-    }, (error) => {
-      $('.showErrors').html(`There was an error processing your request: ${error.message}`);
-    });
+    bike.generateList(make, model, year);
   });
 
   $("#clickGraph").click(() => {
-    bike.generateChart();
-    
+    bike.generateData();
+
     let data = google.visualization.arrayToDataTable([
       ['Bike Color', '# of Bikes'],
       ['Black', bike.black_bikes],
